3c2b8b35d0678364494008f31164c149491a0f45
Jonathan Lopez <jonathan_lopez@bhd.com.do>
2026-02-04 09:41:02 -0400

    - Ajuste del prisma cliente para el error en el build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL as string;
 const adapter = new PrismaPg({ connectionString: DATABASE_URL })
