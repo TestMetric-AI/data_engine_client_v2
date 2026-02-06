9b4153fca0af8f379c0ee692609fb7f4bf837197
Jonathan Lopez <jonathan_lopez@bhd.com.do>
2026-02-06 13:07:25 -0400

    - Mejoras de seguridad

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { SessionExpirationWarning } from "./SessionExpirationWarning";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
+    return (
+        <SessionProvider>
+            {children}
+            <SessionExpirationWarning />
+        </SessionProvider>
+    );
 }
